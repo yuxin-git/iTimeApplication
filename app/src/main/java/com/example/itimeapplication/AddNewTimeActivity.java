e9f580c4174b0e7a63d0b033cf38bb79ca8b224e
yuxin huang <437873566@qq.com>
2019-11-09 15:21:20 +0800

    在新建time页面添加了一个listview用于选择时间，周期等，但还未实现相应的点击事件

--- app/src/main/java/com/example/itimeapplication/AddNewTimeActivity.java
+++ app/src/main/java/com/example/itimeapplication/AddNewTimeActivity.java
@@ -1,17 +1,35 @@
 package com.example.itimeapplication;
 
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.itimeapplication.data.model.Condition;
+
+import java.util.ArrayList;
 
 public class AddNewTimeActivity extends AppCompatActivity {
 
     private EditText editTextName,editTextDescription;
     private Button buttonBack,buttonOk;
+
+    private ArrayList<Condition> conditions=new ArrayList<Condition>();
+    private ConditionsArrayAdapter theConditionAdapter;
+    ListView listViewCondition;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,5 +59,47 @@ public class AddNewTimeActivity extends AppCompatActivity {
                 AddNewTimeActivity.this.finish();
             }
         });
+
+        InitData();
+        theConditionAdapter=new ConditionsArrayAdapter(this,R.layout.list_item_extra_condition, conditions);
+
+        listViewCondition= this.findViewById(R.id.list_view_conditions);
+        listViewCondition.setAdapter(theConditionAdapter);
+    }
+
+    private void InitData()
+    {
+        conditions.add(new Condition(R.drawable.con1,"    Date","       Long press to use Date Calculator"));
+        conditions.add(new Condition(R.drawable.con2,"    Repeat","       None"));
+        conditions.add(new Condition(R.drawable.con3,"    Picture",""));
+        conditions.add(new Condition(R.drawable.con4,"    Add Label",""));
+    }
+
+    public class ConditionsArrayAdapter extends ArrayAdapter<Condition>
+    {
+        private  int resourceId;
+        public ConditionsArrayAdapter(@NonNull AddNewTimeActivity context, @LayoutRes int resource, @NonNull ArrayList<Condition> objects) {
+            super(context, resource, objects);
+            resourceId=resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater mInflater= LayoutInflater.from(this.getContext());
+            View item = mInflater.inflate(this.resourceId,null);
+
+            ImageView con_pic=(ImageView)item.findViewById(R.id.image_view_pic);
+            TextView con_name = (TextView)item.findViewById(R.id.text_view_name);
+            TextView explain = (TextView)item.findViewById(R.id.text_view_explain);
+
+
+            Condition condition_item = this.getItem(position);
+            con_pic.setImageResource(condition_item.getCon_picture());
+            con_name.setText(condition_item.getCon_name());
+            explain.setText(condition_item.getCon_explain());
+
+            return item;
+        }
     }
 }
---
+++ app/src/main/java/com/example/itimeapplication/data/model/Condition.java
@@ -0,0 +1,40 @@
+package com.example.itimeapplication.data.model;
+
+public class Condition {
+
+    private int con_picture;
+    private String con_name;
+    private String con_explain;
+
+    public Condition(int con_picture, String con_name, String con_explain) {
+        this.con_picture = con_picture;
+        this.con_name = con_name;
+        this.con_explain = con_explain;
+    }
+
+    public int getCon_picture() {
+        return con_picture;
+    }
+
+    public void setCon_picture(int con_picture) {
+        this.con_picture = con_picture;
+    }
+
+    public String getCon_name() {
+        return con_name;
+    }
+
+    public void setCon_name(String con_name) {
+        this.con_name = con_name;
+    }
+
+    public String getCon_explain() {
+        return con_explain;
+    }
+
+    public void setCon_explain(String con_explain) {
+        this.con_explain = con_explain;
+    }
+
+
+}
