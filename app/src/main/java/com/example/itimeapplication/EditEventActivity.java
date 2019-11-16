301e41273e9fadde5d060c73998d7d808ba07693
yuxin huang <437873566@qq.com>
2019-11-16 11:49:53 +0800

    实现了事件编辑界面中点击日期选择弹出一个日历及时钟进行选择，但还未实现长按进行输入时间设定

--- app/src/main/java/com/example/itimeapplication/EditEventActivity.java
+++ app/src/main/java/com/example/itimeapplication/EditEventActivity.java
@@ -6,6 +6,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,18 +16,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.itimeapplication.data.model.OtherCondition;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.time.Year;
 import java.util.ArrayList;
+import java.util.Calendar;
+
 
 public class EditEventActivity extends AppCompatActivity {
 
@@ -38,6 +47,10 @@ public class EditEventActivity extends AppCompatActivity {
     ListView listViewCondition;
 
     private int repeat_day;     //保存周期的天数
+    private DatePickerDialog datePickerDialog;
+    private TimePickerDialog timePickerDialog;
+    private Calendar calendar;
+    private int mYear,mMonth,mDay,mHour,mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,8 +104,9 @@ public class EditEventActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String m= String.valueOf(i);    //调试使用
                 Log.i("点击了一个item ", m);
-                if(i==0){       //选择日期
-                    //待实现，需要自定义一个AlertDialog
+                if(i==0) {       //选择日期
+                    calendar = Calendar.getInstance();
+                    showDailog();
                 }
                 if(i==1){  //选择周期
                     String[] repeat = new String[]{"Weak","Month","Year","Custom","None"};
@@ -131,11 +145,14 @@ public class EditEventActivity extends AppCompatActivity {
                                         .show();
 
                             }
+                            if(which==4)    //不重复
+                                repeat_day=0;
                         }
                     });
                     builder.create().show();
                 }
                 if(i==2){   //选择图片
+
 
                 }
                 if(i==3){   //添加标签
@@ -152,6 +169,61 @@ public class EditEventActivity extends AppCompatActivity {
         otherConditions.add(new OtherCondition(R.drawable.picture,"Picture",""));
         otherConditions.add(new OtherCondition(R.drawable.label,"Add Label",""));
     }
+
+    private void showDailog() {     //日历弹出选择框
+        datePickerDialog=new DatePickerDialog(EditEventActivity.this,R.style.Theme_AppCompat_Light_Dialog,
+                null,calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                DatePicker datePicker = datePickerDialog.getDatePicker();
+                int year = datePicker.getYear();
+                int month = datePicker.getMonth();
+                int day = datePicker.getDayOfMonth();
+                Log.d("测试：year", Integer.toString(year));
+                Log.d("测试：month", Integer.toString(month));
+                Log.d("测试：day", Integer.toString(day));
+                mYear=year;
+                mMonth=month;
+                mDay=day;
+                showTime();  //继续设置时间
+            }
+        });
+        datePickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "CANCEL",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        datePickerDialog.show();
+
+    }
+
+    private void showTime() {       //时间弹出选择框
+        timePickerDialog = new TimePickerDialog(this, R.style.Theme_AppCompat_Light_Dialog,new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Log.d("测试：hour", Integer.toString(hourOfDay));
+                Log.d("测试：minite", Integer.toString(minute));
+                mHour=hourOfDay;
+                mMinute=minute;
+            }
+        },
+                calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+        datePickerDialog.setButton(DialogInterface.BUTTON_POSITIVE, "OK",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        datePickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "CANCEL",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        timePickerDialog.show();
+        timePickerDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    }
+
+
 
     public class ConditionsArrayAdapter extends ArrayAdapter<OtherCondition>
     {
